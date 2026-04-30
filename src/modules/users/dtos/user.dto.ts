48da86012129b244b2c81231991dc213d847909e
alswn-03 <soojooyong@gmail.com>
2026-05-01 02:15:27 +0900

    project 기본 설정

---
+++ src/modules/users/dtos/user.dto.ts
@@ -0,0 +1,45 @@
+// 1. 회원가입 요청 데이터의 설계도를 만듭니다.
+export interface UserSignUpRequest {
+  email: string;
+  name: string;
+  gender: string;
+  birth: Date;
+  address?: string;       // ?가 붙으면 '없을 수도 있음(선택)'이라는 뜻이에요!
+  detailAddress?: string;
+  phoneNumber: string;
+  preferences: number[];
+}
+
+
+// 2. 요청받은 데이터를 우리 시스템에 맞는 데이터로 변환해주는 함수입니다. 
+export const bodyToUser = (body: UserSignUpRequest) => {
+  const birth = new Date(body.birth); //날짜 변환
+
+  return {
+    email: body.email, //필수 
+    name: body.name, // 필수
+    gender: body.gender, // 필수
+    birth, // 필수
+    address: body.address || "", //선택 
+    detailAddress: body.detailAddress || "", //선택 
+    phoneNumber: body.phoneNumber,//필수
+    preferences: body.preferences,// 필수 
+  };
+};
+
+// 3. DB에서 가져온 유저 정보 + 선호 카테고리 목록을 클라이언트 응답 형태로 변환
+export const responseFromUser = ({
+    user,
+    preferences,
+}: {
+    user: any;
+    preferences: any[];
+}) => {
+
+  return {
+    email: user.email,
+    name: user.name,
+    preferCategory: preferences.map((preference) => preference.name)
+  };
+};
+
